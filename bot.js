5ff4d8682b86cb9f7b45f5d1fe18575aa71f556e
Alex Cortelyou <acortelyou@gmail.com>
2018-05-21 14:19:35 -0700

    Show rolls if modifier in effect

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ function commandHandler(relThis, command){
 
   msg += ": " + sum;
 
-  if (count > 1 && count < 50) {
+  if ((count > 1 || !isNaN(mod)) && count < 50) {
 
     msg += " [" + rolls.join(" ") + "]";
 
